f8baaaa7feb8eb585e73133b9f64f9fb61de4bff
Gaoce <gaocegege@hotmail.com>
2015-06-28 10:59:52 +0800

    update: remove ? item for math

--- content_script/eductionList.js
+++ content_script/eductionList.js
@@ -60,34 +60,3 @@ function gpa_colomn(table, title, colomn){
     var score_avg = score_all / unit_all;
     jQuery(title).parent().append("本部分课程总GPA为：" + gpa_avg.toFixed(2) + "，平均分为：" + score_avg.toFixed(2));
 }
-
-function score2gpa(score){
-    // not quite sure
-    if (score == "成绩"){
-        return "GPA";
-    }
-    if (score >= 95){
-        return 4.30;
-    }else if (score >= 90){
-        return 4.00;
-    }else if (score >= 85){
-        return 3.70;
-    }else if (score >= 80){
-        return 3.30;
-    }else if (score >= 75){
-        return 3.00;
-    }else if (score >= 70){
-        return 2.70;
-    }else if (score >= 67){
-        return 2.30;
-    }else if (score >= 65){
-        return 2.00;
-    }else if (score >= 63){
-        return 1.70;
-    }else if (score >= 60){
-        return 1.00;
-    }else{
-        return 0.00;
-    }
-    return "";
-}
